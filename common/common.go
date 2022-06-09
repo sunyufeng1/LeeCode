c3418b2fca3cbd2ca3426310103443239655bf1a
sunyufeng <578084772@qq.com>
2022-06-09 10:45:01 +0800

    小调整

--- common/common.go
+++ common/common.go
@@ -8,12 +8,13 @@ import (
 	"github.com/sunyufeng1/LeeCode/testLee"
 )
 
-//动态调用方法
-func InvokeObjectMethod(object interface{}, methodName string, args []reflect.Value) []reflect.Value {
+//动态调用 testLee里的方法
+func InvokeObjectMethod(methodName string, args []reflect.Value) []reflect.Value {
+	leeObj := new(testLee.Obj)
 	t1 := time.Now().UnixNano()
-	result := reflect.ValueOf(object).MethodByName(methodName).Call(args)
+	result := reflect.ValueOf(leeObj).MethodByName(methodName).Call(args)
 	for i := 1; i < 10000; i++ {
-		reflect.ValueOf(object).MethodByName(methodName).Call(args)
+		reflect.ValueOf(leeObj).MethodByName(methodName).Call(args)
 	}
 	t2 := time.Now().UnixNano()
 	runTime := (t2 - t1) / 1e6 //time.Since(t1)
@@ -21,14 +22,13 @@ func InvokeObjectMethod(object interface{}, methodName string, args []reflect.Va
 	return result
 }
 
-//区别 这个不带统计
-//args unite testBase 的参数
+//调用 uniteTest里的方法 并把结果 作为另一个地方的调用参数
 func InvokeObjectMethod1(object interface{}, methodName string) {
 	inputs := make([]reflect.Value, 0)
 	result := reflect.ValueOf(object).MethodByName(methodName).Call(inputs)
-	leeObj := new(testLee.Obj)
+
 	//finalResult testLee中方法返回的结果
-	finalResult := InvokeObjectMethod(leeObj, methodName, result)
+	finalResult := InvokeObjectMethod(methodName, result)
 	fmt.Println("testBase result :")
 	for _, value := range finalResult {
 		fmt.Println(value)
